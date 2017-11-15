a749773dca40a60eacee9e4115d74d13d4c40ddd
Niall Cunningham <N.CUNNINGHAM12@nuigalway.ie>
2017-11-15 19:22:33 +0000

    UK 2017

---
+++ src/ge17/Constituency.java
@@ -0,0 +1,33 @@
+package ge17;
+
+public class Constituency {
+	
+	float con;
+	float lab;
+	float lib;
+	float ukip;
+	float green;
+	float SNP;
+	float plaid;
+	float ind;
+	
+
+	
+	public Constituency(float con, float lab, float lib, float ukip, float green, float SNP, float plaid, float ind){
+		
+		this.con = con;
+		this.lab = lab;
+		this.lib = lib;
+		this.ukip = ukip;
+		this.green = green;
+		this.SNP = SNP;
+		this.plaid = plaid;
+		this.ind = ind;
+		
+	}
+	
+	
+	
+	
+	
+}
